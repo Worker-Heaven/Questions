94a88ce3627fc6564faed793dde8098a799b571b
Worker-Heaven <superstar.geekboy@gmail.com>
2019-05-06 18:22:17 -0300

    Fix download.py

--- download.py
+++ download.py
@@ -3,22 +3,29 @@ import sqlite3
 
 def download_as_csv():
     db_name = 'well_map.db'
+    base_path = 'D:\\maps\\'
 
     conn = sqlite3.connect(db_name)
     c = conn.cursor()
 
-    with open('well_map_result.csv', 'w', newline='', encoding='utf-8') as csvfile:
+    with open('well_map_result_local.csv', 'w', newline='', encoding='utf-8') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(['Wells', 'Location'])
 
     for (well, location) in c.execute('SELECT * from result'):
-        print(well, location)
+        # print(well, location)
+        filtered_well_name = ''.join([x for x in well if not x in '/"'])
 
-        with open('well_map_result.csv', 'a+', newline='', encoding='utf-8') as csvfile:
+        path = base_path + filtered_well_name.strip() + '\\' + 'index.html'
+
+        with open('well_map_result_local.csv', 'a+', newline='', encoding='utf-8') as csvfile:
             writer = csv.writer(csvfile)
-            writer.writerow([well, location])
+            writer.writerow([well, path])
     
     conn.close()
 
 if (__name__ == '__main__'):
     download_as_csv()
+
+
+    can you see `http://` or `https://` in 
